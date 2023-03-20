1086fd43effafd78e31820362acec0d74aed807b
Nick Calibey <nick.calibey@gmail.com>
2023-03-20 08:10:46 -0500

    Implement stack and heap memory (video 2)

--- src/cpu.zig
+++ src/cpu.zig
@@ -1,5 +1,39 @@
 const std = @import("std");
 
+// Memory is an array of bytes (u8).
+fn Memory(comptime size: u64) type {
+    return struct {
+        const Self = @This();
+        data: [size]u8 = undefined,
+        stack_cur: u64 = 0,
+
+        pub fn get_ref(self: *Self, ref: u64) u8 {
+            return self.data[self.data.len - ref - 1];
+        }
+
+        pub fn get_ref_rrange(self: *Self, start: u64, length: u64) []u8 {
+            const start_byte = self.data.len - start - 1;
+            return self.data[start_byte .. start_byte + length];
+        }
+
+        pub fn set_ref(self: *Self, ref: u64, val: u8) void {
+            self.data[self.data.len - ref - 1] = val;
+            return;
+        }
+
+        pub fn pop(self: *Self) u8 {
+            const val = self.data[self.stack_cur];
+            self.stack_cur -= 1;
+            return val;
+        }
+
+        pub fn push(self: *Self, val: u8) void {
+            self.stack_cur += 1;
+            self.data[self.stack_cur] = val;
+        }
+    };
+}
+
 pub const Inst = struct {
     op_code: CPU.OpCodes,
     out: u64, // Is always a register number.
@@ -124,3 +158,96 @@ pub const CPU = struct {
         }
     }
 };
+
+// ###########################
+// ## Tests ##################
+
+test "should set value in register" {
+    var insts = [_]Inst{
+        Inst.Set(0, 10),
+    };
+    var cpu = CPU.init();
+    try cpu.run(&insts);
+
+    try std.testing.expectEqual(@as(u64, 10), cpu.registers[0]);
+}
+
+test "should add values in registers" {
+    var insts = [_]Inst{
+        Inst.Set(0, 10),
+        Inst.Set(1, 10),
+        Inst.Add(2, 0, 1),
+    };
+    var cpu = CPU.init();
+    try cpu.run(&insts);
+
+    try std.testing.expectEqual(@as(u64, 20), cpu.registers[2]);
+}
+
+test "should subtract values in registers" {
+    var insts = [_]Inst{
+        Inst.Set(0, 20),
+        Inst.Set(1, 10),
+        Inst.Sub(2, 0, 1),
+    };
+    var cpu = CPU.init();
+    try cpu.run(&insts);
+
+    try std.testing.expectEqual(@as(u64, 10), cpu.registers[2]);
+}
+
+test "should multiply values in registers" {
+    var insts = [_]Inst{
+        Inst.Set(0, 20),
+        Inst.Set(1, 10),
+        Inst.Mul(2, 0, 1),
+    };
+    var cpu = CPU.init();
+    try cpu.run(&insts);
+
+    try std.testing.expectEqual(@as(u64, 200), cpu.registers[2]);
+}
+
+test "should divide values in registers" {
+    var insts = [_]Inst{
+        Inst.Set(0, 20),
+        Inst.Set(1, 10),
+        Inst.Div(2, 0, 1),
+    };
+    var cpu = CPU.init();
+    try cpu.run(&insts);
+
+    try std.testing.expectEqual(@as(u64, 2), cpu.registers[2]);
+}
+
+test "should raise value in register 1 by value in register 2" {
+    var insts = [_]Inst{
+        Inst.Set(0, 2),
+        Inst.Set(1, 5),
+        Inst.Pow(2, 0, 1),
+    };
+    var cpu = CPU.init();
+    try cpu.run(&insts);
+
+    try std.testing.expectEqual(@as(u64, 32), cpu.registers[2]);
+}
+
+test "should push to and pop from stack" {
+    var mem = Memory(100){};
+    mem.push(1);
+    mem.push(2);
+    try std.testing.expect(mem.pop() == 2);
+    mem.push(3);
+    try std.testing.expect(mem.pop() == 3);
+    try std.testing.expect(mem.pop() == 1);
+}
+
+test "should set and get memory from heap" {
+    var mem = Memory(100){};
+    mem.set_ref(10, 11);
+    try std.testing.expect(mem.get_ref(10) == 11);
+    mem.set_ref(0, 10);
+    try std.testing.expect(mem.get_ref(0) == 10);
+    mem.set_ref(10, 10);
+    try std.testing.expect(mem.get_ref(10) == 10);
+}
--- src/main.zig
+++ src/main.zig
@@ -2,13 +2,6 @@ const std = @import("std");
 const Inst = @import("cpu.zig").Inst;
 const CPU = @import("cpu.zig").CPU;
 
-fn Memory(comptime stack_size: u8, comptime heap_size: u8) type {
-    return struct {
-        stack: Stack(stack_size),
-        heap: [heap_size]u8,
-    };
-}
-
 fn Stack(comptime size: u64) type {
     return struct {
         const Self = @This();
@@ -28,87 +21,8 @@ fn Stack(comptime size: u64) type {
     };
 }
 
-pub fn main() anyerror!void {
-    var insts = [_]Inst{
-        Inst.Set(0, 10),
-        Inst.Set(1, 10),
-        Inst.Add(1, 0, 1),
-        Inst.Set(3, 3),
-        Inst.Pow(2, 0, 3),
-        Inst.Sub(4, 2, 2),
-    };
-    Inst.display(&insts);
-    var cpu = CPU.init();
-    try cpu.run(&insts);
-    cpu.registers_state();
-}
+pub fn main() anyerror!void {}
 
-test "should set value in register" {
-    var insts = [_]Inst{
-        Inst.Set(0, 10),
-    };
-    var cpu = CPU.init();
-    try cpu.run(&insts);
-
-    try std.testing.expectEqual(@as(u64, 10), cpu.registers[0]);
-}
-
-test "should add values in registers" {
-    var insts = [_]Inst{
-        Inst.Set(0, 10),
-        Inst.Set(1, 10),
-        Inst.Add(2, 0, 1),
-    };
-    var cpu = CPU.init();
-    try cpu.run(&insts);
-
-    try std.testing.expectEqual(@as(u64, 20), cpu.registers[2]);
-}
-
-test "should subtract values in registers" {
-    var insts = [_]Inst{
-        Inst.Set(0, 20),
-        Inst.Set(1, 10),
-        Inst.Sub(2, 0, 1),
-    };
-    var cpu = CPU.init();
-    try cpu.run(&insts);
-
-    try std.testing.expectEqual(@as(u64, 10), cpu.registers[2]);
-}
-
-test "should multiply values in registers" {
-    var insts = [_]Inst{
-        Inst.Set(0, 20),
-        Inst.Set(1, 10),
-        Inst.Mul(2, 0, 1),
-    };
-    var cpu = CPU.init();
-    try cpu.run(&insts);
-
-    try std.testing.expectEqual(@as(u64, 200), cpu.registers[2]);
-}
-
-test "should divide values in registers" {
-    var insts = [_]Inst{
-        Inst.Set(0, 20),
-        Inst.Set(1, 10),
-        Inst.Div(2, 0, 1),
-    };
-    var cpu = CPU.init();
-    try cpu.run(&insts);
-
-    try std.testing.expectEqual(@as(u64, 2), cpu.registers[2]);
-}
-
-test "should raise value in register 1 by value in register 2" {
-    var insts = [_]Inst{
-        Inst.Set(0, 2),
-        Inst.Set(1, 5),
-        Inst.Pow(2, 0, 1),
-    };
-    var cpu = CPU.init();
-    try cpu.run(&insts);
-
-    try std.testing.expectEqual(@as(u64, 32), cpu.registers[2]);
+test "root" {
+    _ = @import("cpu.zig");
 }
